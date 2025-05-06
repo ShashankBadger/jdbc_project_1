46d4c40ef00f15a99935bf2cae424c4aaf6c4baf
Shashank R <139161896+ShashankBadger@users.noreply.github.com>
2025-05-06 09:31:55 +0530

    Update Program_01.java

--- src/jdbc_project_1/Program_01.java
+++ src/jdbc_project_1/Program_01.java
@@ -1,9 +1,16 @@
 package jdbc_project_1;
+import java.util.Scanner;
 
 public class Program_01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		// Square a number
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int n = input.nextInt();
+		System.out.println("Square of "+n+" is "+(n*n));
 	}
 
 }
